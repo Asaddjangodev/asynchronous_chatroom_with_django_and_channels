6844a41b4cc1831f5a3beff2e36a3a3162ed4f2f
Asadbek <hoshimovaustin0550@gmail.com>
2025-07-22 01:24:41 +0900

    set up base.html

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,44 @@
-from channels.generic.websocket import AsyncWebSockerConsumer
 
+import json
+from channels.generic.websocket import AsyncWebSockerConsumer
+# Импорт базового класса для асинхронного WebSocket-чата
+
+# Создаем класс чат-комнаты (наследуемся от базового класса)
 class ChatRoomConsumer(AsyncWebSocketConsumer):
-    pass
+    # Метод, который срабатывает при подключении нового пользователя
+    async def connect(self):
+        # 1. Достаем название комнаты из URL (например, из ws/chat/lobby/)
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+
+        # 2. Создаем уникальное имя группы для этой комнаты (chat_lobby)
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # 3. Добавляем текущее соединение в группу комнаты
+        # (чтобы потом можно было рассылать сообщения всем в этой комнате)
+        await self.channel_layer.group_add(
+            self.room_group_name,  # Имя группы (chat_lobby)
+            self.channel_name # Уникальный ID этого соединения
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'hello world',
+            }
+        )
+
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(text_date=json.dumps({
+            'tester': tester,
+        }))
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.groud_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        pass
